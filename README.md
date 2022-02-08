6c50fa0615267a436e8dc11e6c04e8fc3f7f9611
Jerry <calljerryli@outlook.com>
2022-02-08 18:29:03 +0800

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# undeRStood
+
+undeRStood is a picture classification tool.
+
+It uses the pictures in the `img_sample` folder as specimens to find similar pictures in the `img_source` folder, and moves the similar pictures to the `img_result` folder.
+
+🏗 **This project is still in the early stages of development.**
+
+## References
+
+* [https://github.com/jerryshell/similars](https://github.com/jerryshell/similars)
+
+## License
+
+[GNU Affero General Public License v3.0](https://choosealicense.com/licenses/agpl-3.0/)
---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use std::ffi::OsString;
+
+fn main() {
+    let img_sample_path_vec = load_image_path_vec("img_sample");
+    println!("{:?}", img_sample_path_vec);
+
+    img_sample_path_vec.iter().for_each(|img_sample_path| {
+        let img_source_path_vec = load_image_path_vec("img_source");
+        img_source_path_vec.iter().for_each(|img_source_path| {
+            print!(
+                "img_sample_path {:?}, img_source_path {:?} ... ",
+                img_sample_path, img_source_path
+            );
+            let get_image_distance_result = similars_lib::get_image_distance_by_path(
+                img_sample_path.clone().into_string().unwrap(),
+                img_source_path.clone().into_string().unwrap(),
+                8,
+                8,
+                false,
+            );
+            match get_image_distance_result {
+                Err(e) => println!("{:?}", e),
+                Ok(distance) => {
+                    println!("distance {:?}", distance);
+                    if distance <= 10 {
+                        let img_source_path = img_source_path.clone().into_string().unwrap();
+                        let filename = img_source_path.split('/').last().unwrap();
+                        std::fs::rename(
+                            img_source_path.clone(),
+                            format!("img_result/{}", filename),
+                        )
+                        .unwrap();
+                    }
+                }
+            }
+        });
+    });
+}
+
+fn load_image_path_vec(path: &str) -> Vec<OsString> {
+    std::fs::read_dir(path)
+        .unwrap()
+        .map(|d| d.unwrap())
+        .filter(|d| d.file_type().unwrap().is_file())
+        .map(|d| d.path().into_os_string())
+        .collect::<Vec<OsString>>()
+}
