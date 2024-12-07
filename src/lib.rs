4b014a1dc408df9523890f5c9389eb915cec9a4a
jerry <calljerryli@outlook.com>
2024-12-07 23:59:47 +0800

    clean code

--- src/lib.rs
+++ src/lib.rs
@@ -1,57 +1,35 @@
-use rand::prelude::SliceRandom;
-use threadpool::ThreadPool;
-
 pub fn run(
     img_sample_path: std::path::PathBuf,
     img_source_path: std::path::PathBuf,
     img_result_path: std::path::PathBuf,
-    n_workers: usize,
     hamming_threshold: usize,
     clean_flag: bool,
 ) {
     let img_sample_path_vec = load_image_path_vec(img_sample_path).unwrap();
     let img_source_path_vec = load_image_path_vec(img_source_path).unwrap();
-
     let progress_max = img_sample_path_vec.len() * img_source_path_vec.len();
     let mut progress_current = 0usize;
-
-    let n_workers = if n_workers == 0 {
-        num_cpus::get()
-    } else {
-        n_workers
-    };
-    println!("n_workers {}", n_workers);
-
-    let pool = ThreadPool::new(n_workers);
-    let (tx, rx) = std::sync::mpsc::channel::<std::path::PathBuf>();
-
     img_sample_path_vec.into_iter().for_each(|img_sample_path| {
-        let img_sample_path = img_sample_path.clone();
+        let img_sample_path_clone = img_sample_path.clone();
+        let img_source_path_vec = img_source_path_vec.clone();
         let img_result_path = img_result_path.clone();
-        let mut img_source_path_vec = img_source_path_vec.clone();
-        img_source_path_vec.shuffle(&mut rand::thread_rng());
-        let tx = tx.clone();
-        pool.execute(move || {
-            handle_img_sample_path(
-                img_sample_path,
-                img_source_path_vec,
-                img_result_path,
-                hamming_threshold,
-                clean_flag,
-                tx,
-            );
-        })
-    });
-    drop(tx);
-
-    for result in rx {
+        handle_img_sample_path(
+            img_sample_path_clone,
+            img_source_path_vec,
+            img_result_path,
+            hamming_threshold,
+            clean_flag,
+        );
         progress_current += 1;
         let progress = progress_current as f32 / progress_max as f32 * 100f32;
-        println!(
+        tracing::info!(
             "{}/{}\t{:.4}%\t{:?}",
-            progress_current, progress_max, progress, result
+            progress_current,
+            progress_max,
+            progress,
+            img_sample_path
         );
-    }
+    });
 }
 
 pub fn load_image_path_vec(
@@ -72,7 +50,6 @@ pub fn handle_img_sample_path(
     img_result_path: std::path::PathBuf,
     hamming_threshold: usize,
     clean_flag: bool,
-    tx: std::sync::mpsc::Sender<std::path::PathBuf>,
 ) {
     img_source_path_vec.iter().for_each(|img_source_path| {
         match similars_lib::image_distance(img_source_path, &img_sample_path, 8, 8) {
@@ -80,25 +57,22 @@ pub fn handle_img_sample_path(
                 if clean_flag {
                     _ = std::fs::remove_file(img_source_path)
                 }
-                eprintln!("get_image_distance_by_path()::error:{:?}", e);
+                tracing::error!("get_image_distance_by_path()::error:{:?}", e);
             }
             Ok(distance) => {
                 if distance <= hamming_threshold {
                     if let Some(filename) = img_source_path.file_name() {
                         let new_path = img_result_path.join(filename);
                         if let Err(e) = std::fs::rename(img_source_path, new_path) {
-                            eprintln!(
+                            tracing::error!(
                                 "rename()::error:{:?},img_result_path:{:?}",
-                                e, img_result_path
+                                e,
+                                img_result_path
                             );
                         }
                     }
                 }
             }
-        }
-
-        if let Err(e) = tx.send(img_source_path.clone()) {
-            eprintln!("sender.send()::error:{:?}", e);
         }
     })
 }
--- src/main.rs
+++ src/main.rs
@@ -5,24 +5,12 @@ use clap::Parser;
 struct Args {
     #[clap(short = 's', long, default_value = "img_sample")]
     img_sample_path: String,
-
     #[clap(short = 'i', long, default_value = "img_source")]
     img_source_path: String,
-
     #[clap(short = 'o', long, default_value = "img_result")]
     img_result_path: String,
-
-    #[clap(
-        short,
-        long,
-        default_value_t = 0,
-        help = "If n_workers is 0, the number of cpu cores is automatically used"
-    )]
-    n_workers: usize,
-
     #[clap(short = 't', long, default_value_t = 10)]
     hamming_threshold: usize,
-
     #[clap(
         long,
         help = "If --clean-flag is explicitly specified, images in the wrong format will be deleted automatically"
@@ -31,12 +19,12 @@ struct Args {
 }
 
 fn main() {
+    tracing_subscriber::fmt::init();
     let args = Args::parse();
     understood::run(
         args.img_sample_path.into(),
         args.img_source_path.into(),
         args.img_result_path.into(),
-        args.n_workers,
         args.hamming_threshold,
         args.clean_flag,
     );
